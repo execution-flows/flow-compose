de8b81e007a8967b0675ebbe62634b363df50dc6
Vinko Buble <vinkobuble@gmail.com>
2025-02-08 15:36:41 -0500

    some tests

--- tests/test_flow_with_nullary_method.py
+++ tests/test_flow_with_nullary_method.py
@@ -1,3 +1,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import unittest
+
+
+class TestFlowWithNullaryMethod(unittest.TestCase):
+    def test_flow_with_nullary_method(self):
+        pass
