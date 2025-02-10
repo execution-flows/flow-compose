ddd88525520eeb33eaf73b36b738dbff42962924
Vinko Buble <vinkobuble@gmail.com>
2025-02-09 22:22:13 -0500

    Fully functional framework

---
+++ src/flow_compose/__init__.py
@@ -0,0 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from flow_compose.implementation.flow import annotation as flow
+from flow_compose.implementation.flow_function import annotation as flow_function
+
+
+__all__ = ["flow", "flow_function"]
---
+++ tests/test_flow_with_function_composing_another_function_with_arguments_with_default_values.py
@@ -0,0 +1,58 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import unittest
+from unittest.mock import Mock
+
+from flow_compose import flow, flow_function
+from flow_compose.types import FlowFunction
+
+greet_hello_world_mock_1 = Mock()
+greet_hello_world_mock_2 = Mock()
+greet_using_greeting_index_default_mock = Mock()
+greet_using_greeting_mock = Mock()
+
+
+@flow_function()
+def greeting_hello_world_1(index: int) -> str:
+    greet_hello_world_mock_1(index)
+    return f"Hello World! - {index}"
+
+
+@flow_function()
+def greeting_hello_world_2(index: int) -> str:
+    greet_hello_world_mock_2(index)
+    return f"Hello World! - {index}"
+
+
+@flow_function()
+def greet_using_greeting(
+    index: int, index_default: int = 13, greeting=greeting_hello_world_2
+) -> None:
+    greet_using_greeting_index_default_mock(index_default)
+    greet_using_greeting_mock(greeting(index))
+
+
+@flow(
+    greeting=greeting_hello_world_1,
+)
+def hello_world(greet: FlowFunction[None] = greet_using_greeting) -> None:
+    greet(index=11)
+
+
+@flow()
+def hello_world_2(greet: FlowFunction[None] = greet_using_greeting) -> None:
+    greet(index=11)
+
+
+class TestFlowWithFunctionComposingAnotherFunctionWithArgumentsWithDefaultValues(
+    unittest.TestCase
+):
+    def test_flow_with_function_using_another_function(self):
+        hello_world()
+        greet_using_greeting_index_default_mock.assert_called_once_with(13)
+        greet_using_greeting_mock.assert_called_once_with("Hello World! - 11")
+        greet_hello_world_mock_1.assert_not_called()
+        greet_hello_world_mock_2.assert_called_once_with(11)
+
+        hello_world_2()
