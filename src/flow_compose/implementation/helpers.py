989fade6f6aed56db98619430a5e238a17e3c93c
Vinko Buble <vinkobuble@gmail.com>
2025-02-16 14:16:04 -0500

    handle checking non-class type

--- src/flow_compose/implementation/helpers.py
+++ src/flow_compose/implementation/helpers.py
@@ -3,28 +3,21 @@
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import inspect
 from typing import get_origin, Any
-import sys
 
 
 def is_parameter_subclass_type(
     parameter: inspect.Parameter, class_type: type[Any]
 ) -> bool:
-    if callable(parameter.annotation) and hasattr(
-        parameter.annotation, "__supertype__"
-    ):
-        return False
-
-    if sys.version_info >= (3, 12):
-        from typing import TypeAliasType
-
-        if isinstance(parameter.annotation, TypeAliasType):
-            return False
-
     parameter_origin = get_origin(parameter.annotation)
-    return (
-        parameter_origin is not None
-        and issubclass(parameter_origin, class_type)
-        or parameter_origin is None
-        and issubclass(parameter.annotation, class_type)
-        or isinstance(parameter.default, class_type)
-    )
+    try:
+        return (
+            parameter_origin is not None
+            and issubclass(parameter_origin, class_type)
+            or parameter_origin is None
+            and issubclass(parameter.annotation, class_type)
+            or isinstance(parameter.default, class_type)
+        )
+    except TypeError as err:
+        if str(err) == "issubclass() arg 1 must be a class":
+            return False
+        raise err
